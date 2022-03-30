c0ac6bb3498f2ad68fa7e821a364e8ce5571e147
Oyenekan Esther <Oyenekanesther1@gmail.com>
2022-03-30 12:15:09 +0100

    Add files via upload
    This is a QrCode project that i did. If it is scanned It goes to the website 'IVY for president'

---
+++ Main.py
@@ -0,0 +1,15 @@
+import qrcode
+
+data = 'Vote IVY BARLEY for President'
+
+qr = qrcode.QRCode(version = 1, box_size=10, border=5)
+
+qr.add_data(data)
+
+qr.make(fit=True)
+img = qr.make_image(fill_color = 'purple', back_color = 'white')
+
+img.save('C:/Users/USER/Documents/Qrcode/project/myqrcode1.png')
+
+
+
