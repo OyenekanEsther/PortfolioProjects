1f7a1fc2a996e640bc1727d2018a79d3a466c841
Oyenekan Esther <Oyenekanesther1@gmail.com>
2022-03-30 21:22:39 +0100

    Add files via upload
    This my sql lesson

---
+++ sql lesson.sql
@@ -0,0 +1,10 @@
+ CREATE TABLE Student (
+  student_id INT,
+  name VARCHAR(20),
+  major VARCHAR(20),
+  PRIMARY KEY(student_id)
+);
+SELECT * FROM Student;
+
+INSERT INTO Student  VALUES(5, 'Mike', 'Accountancy');
+
